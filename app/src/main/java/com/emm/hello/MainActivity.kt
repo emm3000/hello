1ce7ae5c3ae7de32294e2924bb88fc92cd2734ae
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-01-22 14:41:43 -0500

    chore: Update dependencies
    This commit updates the following dependencies:
    
    - Kotlin to 2.1.0
    - Compose BOM to 2025.01.00

--- app/src/main/java/com/emm/hello/MainActivity.kt
+++ app/src/main/java/com/emm/hello/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.emm.hello.ui.theme.HelloTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
