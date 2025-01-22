c5dac4e415a713944e0cbd2e68864f61b7ecc72b
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-01-22 15:46:48 -0500

    chore: Upload debug APK to Firebase App Distribution
    This commit modifies the workflow to upload the debug APK to Firebase App Distribution instead of the release APK.
    
    - Updated the `assembleRelease` step in the workflow to `assembleDebug`.
    - Updated the path to the APK in the `prodDebugPath` step to point to the debug APK.
    - Renamed the `upload RELEASE DEBUG (APP DISTRIBUTION)` step to `upload DEBUG (APP DISTRIBUTION)`.
    - Updated the app's main activity to display "H E L L O".

--- app/src/main/java/com/emm/hello/MainActivity.kt
+++ app/src/main/java/com/emm/hello/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.emm.hello.ui.theme.HelloTheme
@@ -21,7 +23,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             HelloTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(Modifier.padding(innerPadding))
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Text("H E L L O")
+                    }
                 }
             }
         }
