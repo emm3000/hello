34b493a7c0d95396182a6b96c63855f2ca0942f3
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-01-24 21:40:43 -0500

    feat: Implement Home Screen
    This commit introduces the Home Screen to the application:
    
    - Created `HomeScreen.kt` with a basic layout.
    - Added a centered title "H O M E   S C R E E N".
    - Implemented a button labeled "Ingresar" to go to another screen.
    - Updated `MainActivity` to display the `HomeScreen`.
    - Removed placeholder "H E L L O ZZZ" text from `MainActivity`.
    - Updated the theme to the initial configuration.
    - Added preview for `HomeScreen`.

--- app/src/main/java/com/emm/hello/MainActivity.kt
+++ app/src/main/java/com/emm/hello/MainActivity.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.emm.hello.page.HomeScreen
 import com.emm.hello.ui.theme.HelloTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,23 +19,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             HelloTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding),
-                        contentAlignment = Alignment.Center,
-                    ) {
-                        Text("H E L L O ZZZ")
-                    }
+                    HomeScreen(
+                        onClick = {},
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HelloTheme {
     }
 }
