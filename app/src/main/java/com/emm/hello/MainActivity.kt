da8bc03f2e4bb57e0bdd12971b822a2b674b0f3b
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-01-24 20:36:55 -0500

    feat: Update greeting message in MainActivity
    This commit updates the greeting message displayed in the app's main activity:
    
    - Changed the text from "H E L L O" to "H E L L O ZZZ".

--- app/src/main/java/com/emm/hello/MainActivity.kt
+++ app/src/main/java/com/emm/hello/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
                             .padding(innerPadding),
                         contentAlignment = Alignment.Center,
                     ) {
-                        Text("H E L L O")
+                        Text("H E L L O ZZZ")
                     }
                 }
             }
