9732e3301027993ec27002b00d26f0bfe5ce3fac
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-01-24 23:08:43 -0500

    feat: Integrate Room database and KSP
    This commit integrates the Room persistence library and Kotlin Symbol Processing (KSP) into the project:
    
    - Added Room dependencies for runtime, compiler, and KTX support.
    - Configured KSP plugin to be used in the `data` module.
    - Updated `libs.versions.toml` with Room and KSP versions and dependencies.
    - Updated `build.gradle.kts` to apply KSP plugin in the root and `data` module.
    - Updated `data/build.gradle.kts` to include room dependencies and KSP.

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -37,4 +38,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
 }
