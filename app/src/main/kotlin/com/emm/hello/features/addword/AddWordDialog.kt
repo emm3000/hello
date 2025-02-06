912fa49de905578f7a5e0a6fb8f771d75a05d841
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-02-06 10:42:08 -0500

    feat: Enhance UI/UX and add word feature
    This commit introduces several UI/UX enhancements and adds a new word feature:
    
    -   **Detail Screen:**
        -   Replaced the "Generate content" button with a more prominent "Generate content" button.
        -   Added a "Loading..." animation using letters that float.
        -   Added a message "Content not found, can you generate content?" when content is not found.
    -   - Added `LettersLoading` composable.
    -   - Added `LettersLoading` with animations and message.
        - Replaced `Button` with `OutlinedButton` for generating content.
    -   - Removed old `Button` for generate content
        - Use `MaterialTheme.colorScheme.primary` for the loading animation.
        - Added padding and alignment to the loading animation and generate content elements.
        - Added `wordId`, `pos` and `examples` to `DetailUiState`.
        - Removed placeholder data from `DetailScreenPreview`.
    -   - Refactored `AddWordDialog`.
    +
    +-   **Add Word Dialog:**
    +    -   Enhanced the dialog design with a title: "🆕 New Word".
    +    -   Replaced the `TextField` with `OutlinedTextField`.
    +    -   Improved the placeholder and label text.
    +    -   Updated button texts to "Cancelar" and "Agregar" with bold font and bigger size.
    +    -   Modified colors and background of dialog.
    +    -   Added `DialogProperties` to the dialog.
    +    -   Updated `AddWordDialogPreview` to have a dialog in a column.
    +
    +- Fixed dependencies issues that caused errors like: Unresolved reference: kotlinx.

--- app/src/main/kotlin/com/emm/hello/features/addword/AddWordDialog.kt
+++ app/src/main/kotlin/com/emm/hello/features/addword/AddWordDialog.kt
@@ -8,20 +8,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.emm.hello.core.theme.HelloTheme
 
 @Composable
@@ -35,37 +41,50 @@ fun AddWordDialog(
         modifier = modifier
             .fillMaxWidth(0.85f)
             .clip(RoundedCornerShape(10))
-            .background(MaterialTheme.colorScheme.surfaceVariant)
+            .background(MaterialTheme.colorScheme.surfaceContainer)
             .padding(20.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = "Add word",
+            modifier = Modifier.fillMaxWidth()
+                .padding(bottom = 15.dp),
+            text = "🆕 New Word",
             fontWeight = FontWeight.Bold,
             fontFamily = FontFamily.SansSerif,
             textAlign = TextAlign.Center,
+            fontSize = 20.sp,
             color = MaterialTheme.colorScheme.onSurfaceVariant,
         )
 
         val (word, setWord) = remember {
             mutableStateOf("")
         }
-        TextField(
+        OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
             value = word,
             onValueChange = setWord,
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+
+            ),
             placeholder = {
                 Text(
                     text = "insert new word",
-                    color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f)
+                    color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f),
+                    fontSize = 15.sp
                 )
             },
             label = {
                 Text(
-                    text = "Word",
+                    text = "New Word",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp
                 )
-            }
+            },
+            textStyle = TextStyle(
+                fontSize = 17.sp
+            )
         )
         val keyboardController = LocalSoftwareKeyboardController.current
         Row(
@@ -79,7 +98,9 @@ fun AddWordDialog(
             ) {
                 Text(
                     text = "Cancelar",
-                    color = MaterialTheme.colorScheme.error
+                    color = MaterialTheme.colorScheme.tertiary,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp
                 )
             }
             TextButton(
@@ -90,7 +111,9 @@ fun AddWordDialog(
             ) {
                 Text(
                     text = "Agregar",
-                    color = MaterialTheme.colorScheme.primary
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp
                 )
             }
         }
@@ -101,9 +124,18 @@ fun AddWordDialog(
 @Composable
 private fun AddWordDialogPreview() {
     HelloTheme {
-        AddWordDialog(
-            onDismiss = {},
-            onAddWord = {}
-        )
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Dialog(
+                onDismissRequest = {},
+                properties = DialogProperties(usePlatformDefaultWidth = false)
+            ) {
+                AddWordDialog(
+                    onDismiss = {},
+                    onAddWord = {}
+                )
+            }
+        }
     }
 }
--- app/src/main/kotlin/com/emm/hello/features/detail/DetailScreen.kt
+++ app/src/main/kotlin/com/emm/hello/features/detail/DetailScreen.kt
@@ -1,16 +1,24 @@
 package com.emm.hello.features.detail
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.StartOffset
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -19,19 +27,19 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.icons.outlined.Edit
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -41,11 +49,10 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.emm.domain.Example
-import com.emm.domain.WordContent
 import com.emm.hello.core.theme.HelloTheme
 
 @Composable
@@ -106,22 +113,44 @@ fun DetailScreen(
         )
 
         if (state.hasContent.not()) {
-            Button(
-                modifier = Modifier.fillMaxWidth(),
-                enabled = state.hasContent.not() && state.isLoading.not(),
-                onClick = generateContent
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 Text(
-                    text = "Generate content",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .padding(bottom = 15.dp),
+                    text = "Content not found, can you generate content?",
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontSize = 24.sp,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.ExtraBold
                 )
-                Spacer(Modifier.width(10.dp))
-                if (state.isLoading) {
-                    CircularProgressIndicator(
-                        modifier = Modifier.size(30.dp),
-                        color = LocalContentColor.current
-                    )
+
+                Box(
+                    modifier = Modifier
+                        .padding(bottom = 150.dp)
+                        .height(70.dp),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    if (state.isLoading) {
+                        LettersLoading()
+                    } else {
+                        OutlinedButton(
+                            onClick = generateContent,
+                            shape = RoundedCornerShape(20)
+                        ) {
+                            Text(
+                                text = "Generate content",
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 16.sp,
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -201,6 +230,37 @@ fun DetailScreen(
 }
 
 @Composable
+private fun LettersLoading() {
+    val letters = "Loading...".toList()
+    Row(
+        modifier = Modifier.padding(16.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        letters.forEachIndexed { index, letter ->
+            val animatedOffset by rememberInfiniteTransition().animateFloat(
+                initialValue = 0f,
+                targetValue = -8f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(durationMillis = 650, easing = FastOutSlowInEasing),
+                    repeatMode = RepeatMode.Reverse,
+                    initialStartOffset = StartOffset(index * 100)
+                )
+            )
+
+            Text(
+                text = letter.toString(),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .offset(y = animatedOffset.dp)
+                    .padding(2.dp),
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+}
+
+@Composable
 private fun EditableIconsButtons(
     setWordField: (TextFieldValue) -> Unit,
     updateWord: (String) -> Unit,
@@ -251,21 +311,21 @@ private fun DetailScreenPreview() {
     HelloTheme {
         DetailScreen(
             state = DetailUiState(
-                contentWord = WordContent(
-                    wordId = "",
-                    word = "gaa",
-                    pos = "gaaa x2",
-                    examples = listOf(
-                        Example(
-                            number = "1",
-                            title = "random title",
-                            sentences = listOf(
-                                "random title d ASLCK AS KNCASL CNals kcas lm",
-                                "random title 3"
-                            )
-                        )
-                    )
-                )
+//                contentWord = WordContent(
+//                    wordId = "",
+//                    word = "gaa",
+//                    pos = "gaaa x2",
+//                    examples = listOf(
+//                        Example(
+//                            number = "1",
+//                            title = "random title",
+//                            sentences = listOf(
+//                                "random title d ASLCK AS KNCASL CNals kcas lm",
+//                                "random title 3"
+//                            )
+//                        )
+//                    )
+//                )
             ),
             wordName = "random word",
             generateContent = {},
