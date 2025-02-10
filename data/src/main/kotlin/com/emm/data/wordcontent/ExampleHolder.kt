7911d66e94236aba29cfce16c44b7a978e7995a8
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-02-10 16:44:02 -0500

    refactor: Remove unused `ExampleHolder` and `WordContentHolder` classes
    This commit removes the `ExampleHolder` and `WordContentHolder` classes, which were not being used in the project.
    
    - Removed the `ExampleHolder.kt` file.
    - Removed the `WordContentHolder.kt` file.

--- data/src/main/kotlin/com/emm/data/wordcontent/ExampleHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.emm.data.wordcontent
-
-data class ExampleHolder(
-    val number: String,
-    val title: String,
-    val sentences: List<String>,
-)
--- data/src/main/kotlin/com/emm/data/wordcontent/WordContentHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.emm.data.wordcontent
-
-data class WordContentHolder(
-    val wordFromScrap: String,
-    val pos: String,
-    val examples: List<ExampleHolder>,
-)
