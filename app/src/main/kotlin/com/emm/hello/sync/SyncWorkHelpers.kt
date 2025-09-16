37ae5ceae425ef3d2bd7b7eaec2d7945ce4f66a5
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-09-15 23:05:10 -0500

    refactor: Require charging for sync work
    This commit updates the `SyncConstraints` in `SyncWorkHelpers.kt` to require the device to be charging for sync work to proceed.

--- app/src/main/kotlin/com/emm/hello/sync/SyncWorkHelpers.kt
+++ app/src/main/kotlin/com/emm/hello/sync/SyncWorkHelpers.kt
@@ -13,6 +13,7 @@ import com.emm.hello.R
 val SyncConstraints
     get() = Constraints.Builder()
         .setRequiredNetworkType(NetworkType.CONNECTED)
+        .setRequiresCharging(true)
         .build()
 
 fun Context.syncForegroundInfo(channelName: String, notificationId: Int, channelId: String) = ForegroundInfo(
