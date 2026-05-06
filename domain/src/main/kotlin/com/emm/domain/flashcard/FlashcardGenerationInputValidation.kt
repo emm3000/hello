2c68f0d68b635f25aa19b9950d44eed8437b70f7
Edgardo Muñoz <edgardo.emm20@gmail.com>
2026-05-06 16:50:59 -0500

    chore(domain): remove unused FlashcardGenerationInputValidation typealias

--- domain/src/main/kotlin/com/emm/domain/flashcard/FlashcardGenerationInputValidation.kt
+++
@@ -1,8 +0,0 @@
-package com.emm.domain.flashcard
-
-import com.emm.domain.validation.ValidationIssue
-import com.emm.domain.validation.ValidationResult
-
-typealias FlashcardGenerationInputValidation = ValidationResult<FlashcardGenerationInput>
-
-typealias FlashcardGenerationInputIssue = ValidationIssue
